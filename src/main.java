0da1e1ec1a4d52fb4d367406aecbda68cd2f57db
BuildTools <unconfigured@null.spigotmc.org>
2023-01-17 16:17:59 +0800

    讀取 檔案 新增檔案 開始/結束 設置

--- src/main.java
+++ src/main.java
@@ -2,17 +2,48 @@ import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
+import mobs.mobs;
+import npc.equip;
+import npc.skill;
+
 public class main {
     public static void main(String[] args) throws Exception {
 
         // 數值設定 (回合) / (玩家死亡)
         int round = 0;
         boolean player_survive = true;
+
         // 設置 物件
+        equip equip = new equip();
+        skill skill = new skill();
+        events event = new events();
+        mobs mobs = new mobs();
         create create = new create();
         // 初始化/玩家屬性
 
+        // 讀取檔案 確保檔案是否遺失
         create.read();
+        while ((boolean) player_survive == true) {
+            // 開始遊戲 輸入
+            System.out.println("\n" + "\n" + "點擊 ENTER 來開始遊戲");
+            Scanner start_game_text = new Scanner(System.in);
+            String start_game = start_game_text.nextLine();
+
+            // 玩家死亡
+            player_survive = false;
+
+            // 死亡是否 重新 or 結束遊戲 輸入
+            System.out.println("\n" + "\n" + "你已死亡" + "\n" + "輸入 Y 來重新開始");
+            Scanner start_restart_text = new Scanner(System.in);
+            String start_restart = start_game_text.nextLine();
+            if (start_restart.equals("Y") || start_restart.equals("y")) {
+                System.out.println("你選擇 重新開始遊戲");
+                player_survive = true;
+            } else {
+                System.out.println("\n" + "遊戲結束");
+            }
+        }
+
         // 設置玩家名稱
         /*
          * while (player_survive) {
