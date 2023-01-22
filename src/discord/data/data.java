b26703d4d87c5382a06b4f54f3a3117ca2a83fef
嘿夜 <blacknight0981@gmail.com>
2023-01-22 17:18:51 +0800

    modified:   src/discord/data/data.java

--- src/discord/data/data.java
+++ src/discord/data/data.java
@@ -2,7 +2,8 @@ package discord.data;
 
 public class data {
     // 機器人 token
-    public static final String token = "MTA2MzY0NjU3NzMxMjM5OTQ0Mw.GP5oBp.zet_DszMYbDXNO2IZqOcl4h9QtKuGuExTl5qBU";
+    // 測試
+    public static final String token = "TOKEN";
 
     public static final String CMD_PREFIX = "!";
 }
